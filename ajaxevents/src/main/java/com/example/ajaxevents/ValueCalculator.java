ae229036a523a20e3d47c1626e0e5272a924cee1
Robert Piasecki <amarant1984@gmail.com>
2014-01-31 18:00:35 +0100

    Added very slow algorithm for calculating pi.

--- ajaxevents/src/main/java/com/example/ajaxevents/ValueCalculator.java
+++ ajaxevents/src/main/java/com/example/ajaxevents/ValueCalculator.java
@@ -7,21 +7,26 @@ import javax.inject.Named;
 @Named
 @SessionScoped
 public class ValueCalculator implements Serializable {
-    
+
     private static final long serialVersionUID = 23487237489324L;
-    
     private Double pi = null;
-    
+
     public String calculatePi() throws InterruptedException {
-        Thread.sleep(2000);
-        pi = Math.PI;
+        double sum = 0;
+        double mul = 1;
+        for (int i = 1; i < 100000000; i++) {
+            sum += mul / (2 * i - 1);
+            mul = -mul;
+        }
+        
+        pi = 4 * sum;
         return null;
     }
-    
+
     public Double getPiValue() {
         return pi != null ? pi : 0;
     }
-    
+
     public boolean getHasPiValue() {
         return pi != null;
     }
