4d21d304766853ebb1c1db0bf8b577ac1b2cd1ba
Robert Piasecki <amarant1984@gmail.com>
2014-04-18 14:55:19 +0200

    Added jsf ui:repeat example.

---
+++ jsfrepeat/src/main/java/com/example/jsfrepeat/Headers.java
@@ -0,0 +1,39 @@
+package com.example.jsfrepeat;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+
+@Named
+@RequestScoped
+public class Headers {
+
+    private List<HeaderEntry> entries;
+    
+    @PostConstruct
+    public void init() {
+        entries = new ArrayList<>();
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) context.getRequest();
+        
+        Enumeration<String> namesIt = request.getHeaderNames();
+        while (namesIt.hasMoreElements()) {
+            String name = namesIt.nextElement();
+            Enumeration<String> valueIt = request.getHeaders(name);
+            while (valueIt.hasMoreElements()) {
+                String value = valueIt.nextElement();
+                entries.add(new HeaderEntry(name, value));
+            }
+        }
+    }
+    
+    public List<HeaderEntry> getEntries() {
+        return entries;
+    }
+}
